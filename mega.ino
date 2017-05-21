f81169bdb4a4aa63eab8f82a6db9fd57d84d1038
Jeff Makey <jeff@makey.net>
2017-05-21 16:24:37 -0700

    Use a global boolean to specify whether or not to use the serial console. Use a new subroutine to print debugging messages to the serial console. Initialize the network connection as disconnected. Explicitly initialize the network data string as empty. Wait silently to make a network connection.

--- mega.ino
+++ mega.ino
@@ -6,24 +6,38 @@ IPAddress brain(169,254,136,0);
 
 byte node_number = 5;	// TODO: get this from eeprom
 
+boolean serial0_is_enabled;	// cannot detect at run time
 int led_state;
 unsigned long loop_start_time_msec;
 unsigned long next_connect_msec;
 
 EthernetClient remote;
 
-void setup() {
-  // Open serial communications and wait for port to open:
-  Serial.begin(115200);
-  //while (!Serial) {
-  //  delay(10);	// wait for Leonardo serial port to connect
-  //}
+void print_status(const char* status)
+{
+  if (serial0_is_enabled) {
+    Serial.print(status);
+    Serial.print(" at ");
+    Serial.print(double(millis()) / 1000.0, 2);
+    Serial.println(" seconds");
+  }
+}
 
+void setup() {
+  serial0_is_enabled = false;	// change to true for debugging
+  if (serial0_is_enabled) {
+    Serial.begin(115200);
+  } else {
+    Serial.end();
+  }
+
+  // turn off the LED
   pinMode(LED_BUILTIN, OUTPUT);
   led_state = LOW;	// off
   digitalWrite(LED_BUILTIN, led_state);
 
-  if (Serial) {
+  // show any EEPROM bytes that are not the default value
+  if (serial0_is_enabled) {
     for (int addr = 0; addr < 512; addr++) {
       byte val = EEPROM.read(addr);
       if (val != 255) {
@@ -35,42 +49,39 @@ void setup() {
     }
   }
 
-  // MAC address and IP address of this device
+  // establish MAC address and IP address of this device
   byte mac[] = { 0x35, 0x28, 0x35, 0x28, 0x00, node_number };
   IPAddress self = brain;	// same network
   self[3] = node_number;	// unique host
   Ethernet.begin(mac, self);	// initialize Ethernet shield
+  remote.stop();		// initialize connection state as disconnected
 
-  if (Serial) {
+  if (serial0_is_enabled) {
     Serial.print("node ");
     Serial.print(node_number, DEC);
-    Serial.print(" initialization complete at ");
-    Serial.print(double(millis()) / 1000.0, 2);
-    Serial.println(" seconds");
+    print_status(" initialization complete");
   }
 
   next_connect_msec = 500 + millis();	// 1/2 second in the future; TODO: randomize
 }
 
-void loop()
+void loop()	// up to 13,000 loops per second
 {
   loop_start_time_msec = millis();
   if (remote.connected()) {
     int len = remote.available();
     if (len > 0) {
-      String msg;
+      String msg = "";
       msg.reserve(len + 1);	// pre-allocate the needed space
       for (int i=0; i < len; i++) {
         msg += char(remote.read());
       }
-      if (Serial) {
-        Serial.print("client received '");
-        Serial.print(msg);
-        Serial.print("' at ");
-        Serial.print(millis() / 1000, DEC);
-        Serial.println(" seconds");
+      String editorial;
+      if (serial0_is_enabled) {
+        editorial = "received '" + msg + "'";
+        print_status(editorial.c_str());
       }
-      String editorial = "acknowledged: " + msg;
+      editorial = "acknowledged: " + msg;
       remote.print(editorial.c_str());
 
       if (led_state == LOW) {
@@ -82,30 +93,18 @@ void loop()
     }
   } else {
     if (remote) {
-      if (Serial) {
-        Serial.println("disconnected");
-      }
+      print_status("disconnected from brain");
       remote.stop();
       next_connect_msec = 10000 + loop_start_time_msec;	// 10 seconds in the future; TODO: randomize
     } else if (loop_start_time_msec >= next_connect_msec) {
       if (remote.connect(brain, 3528)) {
-        if (Serial) {
-          Serial.print("connected, my IP address is ");
-          Serial.println(Ethernet.localIP());
-        }
+        print_status("connected to brain");
         String hello = "I am node " + String(node_number, DEC) + " at " + String(millis() / 1000, DEC) + " seconds";
         remote.print(hello.c_str());
       } else {
-        if (Serial) {
-          Serial.println("connection failed");
-        }
+        print_status("connection to brain failed");
         next_connect_msec = 10000 + loop_start_time_msec;	// 10 seconds in the future; TODO: randomize
       }
-    } else {
-      if (Serial) {
-        Serial.print(".");
-      }
-      delay(1000);	// 1 sec
     }
   }
 }
