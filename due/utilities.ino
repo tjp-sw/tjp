7af7cf30cebdda937b9e7e1c825dd80b0a42c9d0
Diane <hoffoss@hotmail.com>
2017-06-12 18:05:44 -0700

    Addressed color palette choice, added get_color()
    I'm at home and unfortunately I can't test that the get_color function works as I want it to, but it seems simple enough that it should.

---
+++ due/utilities.ino
@@ -0,0 +1,39 @@
+void overlay() {
+
+  // copy mid layer on top of existing background layer
+//  for  (int ring = 0; ring < RINGS_PER_NODE; ring++) {
+//    for (int pixel = 0; pixel < LEDS_PER_RING; pixel++) {
+//      if (mid_is_set[ring][pixel]) {
+//        leds[ring][pixel] = mid[ring][pixel];
+//      }
+//    }
+//  }
+
+  
+  // copy sparkle layer on top of existing led layer
+  for  (int ring = 0; ring < RINGS_PER_NODE; ring++) {
+    for (int pixel = 0; pixel < LEDS_PER_RING; pixel++) {
+      if (sparkle_is_set[ring][pixel]) {
+        leds[ring][pixel] = sparkle[ring][pixel];
+      }
+    }
+  }
+}
+
+
+//  palette_num from 0 to 2 indicates which of the 3 palettes we're going to use for this animation
+//  color_index from 0 to 6 indicates which of the 7 colors in that palette to use
+CRGB get_color(int palette_num, int color_index) {
+
+  switch (palette_num)  {   
+  case 0:
+    return icy_bright[color_index];
+
+  case 1:
+    return watermelon[color_index];
+
+  case 2:
+    return fruit_loop[color_index];
+  }
+}
+
