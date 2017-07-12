982da4cea6caa531d57cca3c822fbf646728e82d
Kienen <Kienen.mason@gmail.com>
2017-07-11 20:54:44 -0700

    Added Music class
    call Music.tick() to update songs playing

--- tsunami/music.py
+++ tsunami/music.py
@@ -1,20 +1,41 @@
 import random
-from datetime import date, time, datetime, timedelta
+from datetime import datetime, timedelta
+
+
+# Just random....not sure where this signal is coming from
+def panel_touched():
+    chance = random.randint(0, 300000)
+    if chance == 0:
+        return True
+    return False
 
 
 # sends a control message to the mega
 class ControlMessage:
-    def __init__(self, node=0, command=0, field2=0, channels=None):
+    def __init__(self, node=0, command=1, field2=0, channels=None):
         self.node = node
         self.command = command
         self.field2 = field2
-        self.channels = channels
+        if channels is not None:
+            self.channels = channels
+        else:
+            self.channels = [0] * 7
+
+    def add_channel(self, channel, value):
+        self.channels[channel] = value
 
     def send(self):
-        ctrl_msg = ';'.join([str(self.node), str(self.command), str(self.field2),
-                             ','.join(str(ch) for ch in self.channels)])
-        ctrl_msg += ';'
-        print(ctrl_msg)
+        empty_msg = True
+        if self.command == 3:
+            empty_msg = False
+        for ch in self.channels:
+            if ch > 0:
+                empty_msg = False
+        if empty_msg is False:
+            ctrl_msg = ';'.join([str(self.node), str(self.command), str(self.field2),
+                                 ','.join(str(ch) for ch in self.channels)])
+            ctrl_msg += ';'
+            print(ctrl_msg)
 
     def play(self, channels, node=0, looping=0):
         self.node = node
@@ -36,6 +57,7 @@ class ControlMessage:
         self.send()
 
 
+# Stores Music library and returns appropriate songs
 class Songs:
     RED_MID = [1]
     RED_HIGH = [2]
@@ -56,16 +78,13 @@ class Songs:
     LOWS = [15, 16, 17]
 
     def __init__(self, theme=datetime.today().weekday()):
-        self.channels = [0] * 7
-        self.low_is_playing = False
-        self.played_mid = datetime.min
-        self.played_high = datetime.min
-
         self.theme = theme
         self.available_lows = [self.LOWS[theme]]
-        # self.available_mids = []
-        # self.available_highs = []
-        # self.msg = ControlMessage()
+
+    def find_low(self, this_theme=None):
+        if this_theme is None:
+            this_theme = self.theme
+        return self.LOWS[this_theme]
 
     def find_mid(self, this_theme=None):
         if this_theme is None:
@@ -76,66 +95,55 @@ class Songs:
             return random.choice(all_mids)
         return random.choice(self.MIDS[this_theme])
 
-    def find_high(self, this_theme= None):
+    def find_high(self, this_theme=None):
         if this_theme is None:
             this_theme = self.theme
 
         if datetime.now().hour >= 21:
             all_highs = [song for sublist in self.HIGHS for song in sublist]
             return random.choice(all_highs)
-        return random.choice(self.MIDS[this_theme])
+        return random.choice(self.HIGHS[this_theme])
 
 
+class Music:
+    songs = Songs()
 
+    def __init__(self):
+        self.played_low = -1
+        self.played_mid = datetime.min
+        self.checked_high = datetime.min
+        self.played_high = datetime.min
 
+    def tick(self):
+        if self.played_low != datetime.today().weekday():  # Changes at midnight. This probably should be fixed
+            low = self.songs.find_low()
+            low_msg = ControlMessage()
+            low_msg.play([low], looping=1)
+            self.played_low = datetime.today().weekday()
 
-m= Music()
-ctrl= ControlMessage()
+        tick_msg = ControlMessage()
+        if self.played_mid <= (datetime.now() - timedelta(minutes=1)):
+            tick_msg.add_channel(1, self.songs.find_mid())
+            self.played_mid = datetime.now()
+
+        if self.checked_high <= (datetime.now() - timedelta(minutes=1)):
+            play_chance = random.randint(0, 4)
+            if play_chance == 0:
+                tick_msg.add_channel(2, self.songs.find_high())
+                self.played_high = datetime.now()
+            self.checked_high = datetime.now()
+
+        if panel_touched():
+            tick_msg.add_channel(3, self.songs.find_high())
+
+        tick_msg.send()
+
+music = Music()
 
 while True:
-
-print(datetime.now().hour)
-print (m.find_high())
-#ctrl.play([m.find_high()])
-#ControlMessage([3])
+    music.tick()
 
 """  
-class Theme:
-    def __init__(self, chakra):
-        self.low= Songs
-
-m = Music()
- 
-t= Tsunami()
-print("yo")
-     
-def send_ctrl_msg(node, command, field2, channels):
-    ctrl_msg = ';'.join([str(node), str(command), str(field2), ','.join(str (ch) for ch in channels)])
-    ctrl_msg += ';'
-    print(ctrl_msg)
-
-def play_music(node, looping, channels):
-    send_ctrl_msg(node, 1, looping, channels)
-
-def set_volume(node, fade_speed, channels):
-    node
-    ctrl_msg = ';'.join([str(node), '2', str(fade_speed), ','.join(str (ch) for ch in channels)])
-    ctrl_msg += ';'
-    print(ctrl_msg)
-
-
-#returns a random relevant song
-def find_available(this_theme, channel, is_night= 0):
-    return 17
-
-def setup():
-
-    play_music(0, 0, [find_available(theme, 1), find_available(theme, 2)])
-
-#send_music(0, 1,0,[6,7])
-random.seed()
-#print (random.randint(0,23))
-#print(date.today().weekday())
 
 #Set theme
 theme_list= ['RED', 'ORANGE', 'YELLOW', 'GREEN', 'BLUE', 'PURPLE', 'WHITE']
@@ -147,25 +155,4 @@ BLUE= 4
 PURPLE= 5
 WHITE= 6
 
-if (low_is_playing is False):
-    play_music(0, 1, [find_available(theme, 0)])
-    low_is_playing= True
-#print (datetime.now() - timedelta(seconds=30))
-
-
-if (played_mid <= (datetime.now() - timedelta(seconds=30))):
-    play_music(0, 0, [find_available(theme, 1)])
-    played_mid= datetime.now()
-
-if (played_high <= (datetime.now() - timedelta(minutes=1))):
-    play_chance= random.randint(0, 4)
-    if (play_chance == 0):
-        play_music(0, 0, [find_available(theme, 1)])
-    played_high= datetime.now()
-
-
-    if(played_mid):
-        print(played_mid)
-except NameError:
-    print (datetime.now())
 """
